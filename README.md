dd967043993733765b1da279609583c253beeace
Chantae Palacio <chantae.palacio@gmail.com>
2022-07-21 14:38:46 -0700

    Completed Project

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # Etch_A_Sketch
 
-## Resources
-- [How to create 16x16 grid using Flexbox](https://stackoverflow.com/questions/69943484/trying-to-create-16x16-grid-with-flexbox-but-for-some-reason-width-100-wont-w)
+Color Away! 
+
+All you have to do is enter a grid size which ranges between 2 and 64 or stick with the default size of 16x16.
+
+
+# What I learned
+
+- How to use the "mouseover" event listener
+
+- I learned how to generate random colors in two different ways
+
+- I learned about how to reset an element by using the replaceChildren() method
+
+## Resources I used
+
+- [YouTube](https://www.youtube.com/watch?v=dyhuaXeuyGo) - This helped me create the grid display and the rainbow button
+
+- [JavaScript Form Validation](https://www.w3schools.com/js/js_validation.asp) - This helped me create the error message when the user enters a valid that is out of the range of 2- 64
+
+- [How to Generate a Random Color](https://css-tricks.com/snippets/javascript/random-hex-color/) - This helped me to create the code for the random button to get a random color
--- script.js
+++ script.js
@@ -1,7 +1,91 @@
-const cdiv = document.querySelector('.container');
+const container = document.querySelector(".container");
+const childNodes = container.childNodes;
+const input = document.querySelector("input");
+const blackBtn = document.querySelector(".black-btn");
+const rainbowBtn = document.querySelector(".rainbow-btn");
+const randomBtn = document.querySelector(".random-btn");
+const eraseBtn = document.querySelector(".erase-btn");
+const resetBtn = document.querySelector(".reset-btn");
 
-// Add 16 divs
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div');
-  cdiv.appendChild(div);
+const createDivs = (rows,cols) => {
+  container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;  
+  container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+
+  const area = rows * cols;
+  for (let i = 0; i < area; i++) {
+    const div = document.createElement('div');
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    });
+
+    div.style.backgroundColor = "white";
+    container.appendChild(div);
+  }
 }
+
+const changeSize = (input) => {  
+   if (input <= 1 || input > 64) {
+    let error = "Enter a value between 2 and 64";
+    document.getElementById("error").textContent = error;
+  } else if(input >= 2 || input <= 64) {
+    document.getElementById("error").textContent = "";
+    const container = document.querySelector(".container");
+    container.replaceChildren();
+    createDivs(input,input);
+  } 
+}
+
+//Black Button
+const black = () => {
+childNodes.forEach(child => {
+    child.addEventListener("mouseover", () =>{
+      child.style.backgroundColor = "black";
+    })
+  });
+} 
+
+//Rainbow Button
+const rainbow = () => {
+  childNodes.forEach(child => {
+    child.addEventListener("mouseover", () =>{
+      child.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;  
+    })
+  });
+}
+
+//Random Button
+const random = () => {
+  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  let colorDisplay = document.querySelector(".color-display");
+  colorDisplay.style.backgroundColor = `#${randomColor}`;
+
+  childNodes.forEach(child => {
+    child.addEventListener("mouseover", () =>{
+      child.style.backgroundColor = `#${randomColor}`;  
+    })
+  });
+}
+
+//Erase Button
+const erase = () => {
+  childNodes.forEach(child => {
+      child.addEventListener("mouseover", () =>{
+        child.style.backgroundColor = "white";
+      })
+    });
+  } 
+
+//Reset Button
+const reloadPage = () => {
+  window.location.reload();
+}
+
+
+createDivs(16,16);
+
+input.addEventListener("change", changeSize);
+blackBtn.addEventListener("click", black);
+rainbowBtn.addEventListener("click", rainbow);
+randomBtn.addEventListener("click", random);
+eraseBtn.addEventListener("click", erase);
+resetBtn.addEventListener("click", reloadPage);
